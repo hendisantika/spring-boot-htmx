416001e01f092406f3b841165a14232793e1f639
hendisantika <hendisantika@yahoo.co.id>
2025-03-17 23:30:32 +0700

    feat: Add IntentionalErrorController🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/springboothtmx/IntentionalErrorController.kt
@@ -0,0 +1,17 @@
+package id.my.hendisantika.springboothtmx
+
+import org.springframework.stereotype.Controller
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-htmx
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 17/03/25
+ * Time: 23.30
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+class IntentionalErrorController
