aac2dfaf5e4989b3dcc03f563575d58cf3724188
hendisantika <hendisantika@yahoo.co.id>
2025-03-17 23:34:28 +0700

    feat: Add StreamLambdaHandler🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/springboothtmx/StreamLambdaHandler.kt
@@ -0,0 +1,14 @@
+package id.my.hendisantika.springboothtmx
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-htmx
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 17/03/25
+ * Time: 23.34
+ * To change this template use File | Settings | File Templates.
+ */
+class StreamLambdaHandler : RequestStreamHandler
