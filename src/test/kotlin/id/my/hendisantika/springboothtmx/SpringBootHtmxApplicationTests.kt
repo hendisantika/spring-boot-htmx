61695b51ffef4969cdd884543abdc45aedefde31
hendisantika <hendisantika@yahoo.co.id>
2025-03-18 06:47:54 +0700

    feat: Add indexIsServed🫸🌀✏️📗🐧🐳

--- src/test/kotlin/id/my/hendisantika/springboothtmx/SpringBootHtmxApplicationTests.kt
+++ src/test/kotlin/id/my/hendisantika/springboothtmx/SpringBootHtmxApplicationTests.kt
@@ -1,13 +1,26 @@
 package id.my.hendisantika.springboothtmx
 
+import org.hamcrest.core.StringContains
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
 
-@SpringBootTest
+@AutoConfigureMockMvc
+@SpringBootTest(classes = [SpringBootHtmxApplication::class])
 class SpringBootHtmxApplicationTests {
 
-    @Test
-    fun contextLoads() {
-    }
+    @Autowired
+    lateinit var mockMvc: MockMvc
 
+    @Test
+    fun indexIsServed() {
+        mockMvc.get("/")
+            .andExpect {
+                content { string(StringContains("<!doctype html>")) }
+                status { isOk() }
+            }
+    }
 }
