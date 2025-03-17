3a3fb13a84da5bbb299a77d587e9554085c619ea
hendisantika <hendisantika@yahoo.co.id>
2025-03-17 23:34:46 +0700

    feat: Add handleRequest in StreamLambdaHandler🫸🌀✏️📗🐧🐳

--- src/main/kotlin/id/my/hendisantika/springboothtmx/StreamLambdaHandler.kt
+++ src/main/kotlin/id/my/hendisantika/springboothtmx/StreamLambdaHandler.kt
@@ -1,5 +1,10 @@
 package id.my.hendisantika.springboothtmx
 
+import io.micrometer.observation.Observation
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-htmx
@@ -11,4 +16,10 @@ package id.my.hendisantika.springboothtmx
  * Time: 23.34
  * To change this template use File | Settings | File Templates.
  */
-class StreamLambdaHandler : RequestStreamHandler
+class StreamLambdaHandler : RequestStreamHandler {
+    @Throws(IOException::class)
+    override fun handleRequest(inputStream: InputStream, outputStream: OutputStream, context: Observation.Context) {
+        handler!!.proxyStream(inputStream, outputStream, context)
+    }
+
+}
